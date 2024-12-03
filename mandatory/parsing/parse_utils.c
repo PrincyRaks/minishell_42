abf10ad2586f81eb3956188c294194259c115849
Rovasoanirina Princy Rakotosalama <rrakotos@c1r9s13.42antananarivo.mg>
2024-12-03 11:51:56 +0300

    format line function parsing

--- mandatory/parsing/parse_utils.c
+++ mandatory/parsing/parse_utils.c
@@ -6,39 +6,44 @@
 /*   By: rrakotos <rrakotos@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 16:29:31 by rrakotos          #+#    #+#             */
-/*   Updated: 2024/12/02 17:40:00 by rrakotos         ###   ########.fr       */
+/*   Updated: 2024/12/03 11:39:33 by rrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	store_token(t_tokens *node_token, int *is_cmd, char *input)
+static void	store_token(t_tokens *node_token, char **input)
 {
 	t_cmd	*node_cmd;
 
-	if (is_cmd)
+	if (node_token->token_cmd != NULL)
 	{
-		addback_arg(&node_token->token_arg, parse_input(&input));
+		addback_arg(&node_token->token_arg, parse_input(input));
 		return ;
 	}
 	node_cmd = new_cmd();
-	node_cmd->cmd_str = parse_input(&input);
-	if(!node_cmd->cmd_str)
+	node_cmd->cmd_str = parse_input(input);
+	if (!node_cmd->cmd_str)
 		node_cmd->errnum = UNQUOTES;
 	node_token->token_cmd = node_cmd;
-	*is_cmd = 1;
 }
 
-// static void	store_new_token(t_tokens **first_node){}
+static int	create_new_token(t_tokens **first_node)
+{
+	t_tokens	*node_token;
+
+	node_token = new_token();
+	if (!node_token)
+		return (0);
+	addback_token(first_node, node_token);
+	return (1);
+}
 
 t_tokens	**store_instruction(char *input)
 {
-	int			is_cmd;
 	t_tokens	*node_token;
 	t_tokens	**first_node;
-	// t_cmd		*node_cmd;
 
-	is_cmd = 0;
 	first_node = malloc(sizeof(t_tokens *));
 	if (!first_node)
 		return (NULL);
@@ -51,27 +56,11 @@ t_tokens	**store_instruction(char *input)
 		while (*input == ' ')
 			input++;
 		if (*input != ' ' && *input != '\0')
-		{
-			// store_token(node_token, &is_cmd, input);
-			if (!is_cmd)
-			{
-				node_cmd = new_cmd();
-				node_cmd->cmd_str = parse_input(&input);
-				if (!node_cmd->cmd_str)
-					node_cmd->errnum = UNQUOTES;
-				node_token->token_cmd = node_cmd;
-				is_cmd = 1;
-			}
-			else
-				addback_arg(&z->token_arg, parse_input(&input));
-		}
+			store_token(node_token, &input);
 		if (*input == '|')
 		{
-			node_token = new_token();
-			if (!node_token)
-				return (NULL);
-			addback_token(first_node, node_token);
-			is_cmd = 0;
+			if (!create_new_token(first_node))
+				break ;
 			input++;
 		}
 	}
