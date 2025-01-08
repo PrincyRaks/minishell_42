dca8568f755b1e075b57a8bc39b91477fddc7300
mrazanad <mrazanad@c1r9s13.42antananarivo.mg>
2025-01-08 06:20:52 +0300

    debug

--- mandatory/pipe/execute_pipe.c
+++ mandatory/pipe/execute_pipe.c
@@ -6,7 +6,7 @@
 /*   By: mrazanad <mrazanad@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:07:28 by mrazanad          #+#    #+#             */
-/*   Updated: 2025/01/08 05:03:31 by mrazanad         ###   ########.fr       */
+/*   Updated: 2025/01/08 06:16:41 by mrazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void	execute_pipeline(t_tokens *tokens)
 	while (last->next)
 		last = last->next;
 	
-	// Only validate command if it doesn't have redirection
 	if (!last->token_flow && last->token_cmd && !check_command(last))
 	{
 		set_signals_interactive();
@@ -32,21 +31,21 @@ void	execute_pipeline(t_tokens *tokens)
 	
 	prev_fd = -1;
 	current = tokens;
-	printf("\nDEBUG: Starting pipeline execution\n");
+	// printf("\nDEBUG: Starting pipeline execution\n");
 	while (current)
 	{
-		printf("DEBUG: Processing command: %s\n",
-			current->token_cmd ? current->token_cmd->cmd_str : "NULL");
+		// printf("DEBUG: Processing command: %s\n",
+		// 	current->token_cmd ? current->token_cmd->cmd_str : "NULL");
 		execute_command(tokens, current, &prev_fd);
 		current = current->next;
 	}
 	
 	if (prev_fd != -1)
 	{
-		printf("DEBUG: Closing final prev_fd %d\n", prev_fd);
+		// printf("DEBUG: Closing final prev_fd %d\n", prev_fd);
 		close(prev_fd);
 	}
-	printf("DEBUG: Waiting for all children to finish\n");
+	// printf("DEBUG: Waiting for all children to finish\n");
 	while (wait(NULL) > 0)
 		;
 	set_signals_interactive();
--- mandatory/pipe/utils.c
+++ mandatory/pipe/utils.c
@@ -6,7 +6,7 @@
 /*   By: mrazanad <mrazanad@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 11:12:14 by mrazanad          #+#    #+#             */
-/*   Updated: 2025/01/08 05:03:56 by mrazanad         ###   ########.fr       */
+/*   Updated: 2025/01/08 06:17:56 by mrazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,29 +34,26 @@ void	setup_child_process(t_tokens *tokens, t_tokens *current, int prev_fd,
 {
 	(void)tokens;
 	set_signals_noninteractive();
-	printf("\nDEBUG [Child %d]: Command: %s\n", getpid(), 
-		current->token_cmd ? current->token_cmd->cmd_str : "NULL");
-	printf("DEBUG [Child %d]: FDs - prev_fd=%d, pipe[0]=%d, pipe[1]=%d\n", 
-		getpid(), prev_fd, pipe_fd[0], pipe_fd[1]);
+	// printf("\nDEBUG [Child %d]: Command: %s\n", getpid(), 
+	// 	current->token_cmd ? current->token_cmd->cmd_str : "NULL");
+	// printf("DEBUG [Child %d]: FDs - prev_fd=%d, pipe[0]=%d, pipe[1]=%d\n", 
+	// 	getpid(), prev_fd, pipe_fd[0], pipe_fd[1]);
 
-	// Setup stdin from previous pipe
 	if (prev_fd != -1)
 	{
-		printf("DEBUG [Child %d]: Setting stdin from prev_fd %d\n", getpid(), prev_fd);
+		// printf("DEBUG [Child %d]: Setting stdin from prev_fd %d\n", getpid(), prev_fd);
 		if (dup2(prev_fd, STDIN_FILENO) == -1)
 			perror("dup2 prev_fd");
 		close(prev_fd);
 	}
 
-	// Setup stdout to next pipe
 	if (current->next)
 	{
-		printf("DEBUG [Child %d]: Setting stdout to pipe_fd[1] %d\n", getpid(), pipe_fd[1]);
+		// printf("DEBUG [Child %d]: Setting stdout to pipe_fd[1] %d\n", getpid(), pipe_fd[1]);
 		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
 			perror("dup2 pipe_fd");
 	}
 
-	// Close unused pipe ends
 	if (pipe_fd[0] != -1)
 		close(pipe_fd[0]);
 	if (pipe_fd[1] != -1)
@@ -64,7 +61,7 @@ void	setup_child_process(t_tokens *tokens, t_tokens *current, int prev_fd,
 
 	if (current->token_flow)
 	{
-		printf("DEBUG [Child %d]: Applying redirections\n", getpid());
+		// printf("DEBUG [Child %d]: Applying redirections\n", getpid());
 		apply_redirection(current);
 	}
 	execute_single_command(current);
@@ -72,7 +69,6 @@ void	setup_child_process(t_tokens *tokens, t_tokens *current, int prev_fd,
 
 int	check_command(t_tokens *current)
 {
-	// Allow tokens with only redirections
 	if (!current->token_cmd && current->token_flow)
 		return (1);
 
@@ -80,7 +76,6 @@ int	check_command(t_tokens *current)
 		|| (!is_builtin(current->token_cmd->cmd_str)
 			&& !find_executable(current->token_cmd->cmd_str)))
 	{
-		// Show errors only if there's no redirection
 		if (!current->token_flow)
 		{
 			if (current->token_cmd != NULL && current->token_cmd->cmd_str != NULL 
@@ -114,25 +109,25 @@ void	handle_pipe_fds(t_tokens *tokens, t_tokens *current, int *prev_fd,
 		int *pipe_fd)
 {
 	(void)tokens;
-	printf("\nDEBUG [Parent]: Handling FDs for command: %s\n", 
-		current->token_cmd ? current->token_cmd->cmd_str : "NULL");
+	// printf("\nDEBUG [Parent]: Handling FDs for command: %s\n", 
+	// 	current->token_cmd ? current->token_cmd->cmd_str : "NULL");
 
 	if (*prev_fd != -1)
 	{
-		printf("DEBUG [Parent]: Closing prev_fd %d\n", *prev_fd);
+		// printf("DEBUG [Parent]: Closing prev_fd %d\n", *prev_fd);
 		close(*prev_fd);
 	}
 
 	if (current->next)
 	{
-		printf("DEBUG [Parent]: Closing pipe write end %d\n", pipe_fd[1]);
+		// printf("DEBUG [Parent]: Closing pipe write end %d\n", pipe_fd[1]);
 		close(pipe_fd[1]);
 		*prev_fd = pipe_fd[0];
-		printf("DEBUG [Parent]: Saving pipe read end %d for next command\n", pipe_fd[0]);
+		// printf("DEBUG [Parent]: Saving pipe read end %d for next command\n", pipe_fd[0]);
 	}
 	else
 	{
-		printf("DEBUG [Parent]: Last command, closing both pipe ends\n");
+		// printf("DEBUG [Parent]: Last command, closing both pipe ends\n");
 		if (pipe_fd[0] != -1)
 			close(pipe_fd[0]);
 		if (pipe_fd[1] != -1)
@@ -146,19 +141,18 @@ void	execute_command(t_tokens *tokens, t_tokens *current, int *prev_fd)
 	int		pipe_fd[2];
 	pid_t	pid;
 
-	// Create pipe for next command if needed
 	if (current->next)
 	{
-		printf("DEBUG [Parent]: Creating pipe for command: %s\n",
-			current->token_cmd ? current->token_cmd->cmd_str : "NULL");
+		// printf("DEBUG [Parent]: Creating pipe for command: %s\n",
+		// 	current->token_cmd ? current->token_cmd->cmd_str : "NULL");
 		if (pipe(pipe_fd) == -1)
 		{
 			perror("pipe");
 			set_signals_interactive();
 			exit_perror("pipe");
 		}
-		printf("DEBUG [Parent]: New pipe FDs: read=%d, write=%d\n", 
-			pipe_fd[0], pipe_fd[1]);
+		// printf("DEBUG [Parent]: New pipe FDs: read=%d, write=%d\n", 
+		// 	pipe_fd[0], pipe_fd[1]);
 	}
 	else
 	{
@@ -172,7 +166,6 @@ void	execute_command(t_tokens *tokens, t_tokens *current, int *prev_fd)
 	if (pid == 0)
 		setup_child_process(tokens, current, *prev_fd, pipe_fd);
 	
-	// Parent process: close used FDs and setup for next command
 	if (*prev_fd != -1)
 		close(*prev_fd);
 	if (current->next)
